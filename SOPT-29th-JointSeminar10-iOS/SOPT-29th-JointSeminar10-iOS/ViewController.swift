48b970e36ea23ff1e71ebbd1b17d3970043a021f
hyungyu Kim <ypsophila97@icloud.com>
2021-11-13 01:20:12 +0900

    ➕[ADD] 프로젝트 생성 및 설정
    ### Descriptions
    - 프로젝트 생성
    - 이슈 템플릿 생성
    - 풀리퀘 템플릿 생성
    - .gitignore
    - 커밋 메시지 생성

---
+++ SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT-29th-JointSeminar10-iOS
+//
+//  Created by kimhyungyu on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
