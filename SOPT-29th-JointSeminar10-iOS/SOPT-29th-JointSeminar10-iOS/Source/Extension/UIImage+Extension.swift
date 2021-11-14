6f943190de01d45e3218f7a01ce53191223060d0
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-11-14 15:18:03 +0900

    ➕[ADD] 이미지 Extension 추가 (#1)

---
+++ SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/Extension/UIImage+Extension.swift
@@ -0,0 +1,84 @@
+//
+//  UIImage+Extension.swift
+//  SOPT-29th-JointSeminar10-iOS
+//
+//  Created by 정은희 on 2021/11/14.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    class var defaultCarplan: UIImage {
+        return UIImage(named: "default_carplan")!
+    }
+    
+    static var defaultHome: UIImage {
+        return UIImage(named: "default_home")!
+    }
+    
+    class var defaultMypage: UIImage {
+        return UIImage(named: "default_mypage")!
+    }
+    
+    class var defaultPairing: UIImage {
+        return UIImage(named: "default_pairing")!
+    }
+    
+    class var defaultHeartIcon: UIImage {
+        return UIImage(named: "ic_default_heart_ios")!
+    }
+    
+    class var selectedCarplan: UIImage {
+        return UIImage(named: "selected_carplan")!
+    }
+    
+    class var selectedHome: UIImage {
+        return UIImage(named: "selected_home")!
+    }
+    
+    class var selectedMypage: UIImage {
+        return UIImage(named: "selected_mypage")!
+    }
+    
+    class var selectedPairing: UIImage {
+        return UIImage(named: "selected_pairing")!
+    }
+    
+    class var selectedHeartIcon: UIImage {
+        return UIImage(named: "ic_selected_heart_ios")!
+    }
+    
+    class var santafeImage: UIImage {
+        return UIImage(named: "img_싼타페")!
+    }
+    
+    class var allnewmorningImage: UIImage {
+        return UIImage(named: "img_올뉴모닝")!
+    }
+    
+    class var carnivalImage: UIImage {
+        return UIImage(named: "img_카니발11인승")!
+    }
+    
+    class var allnewk3Image: UIImage {
+        return UIImage(named: "img_allnewk3")!
+    }
+    
+    class var conaImage: UIImage {
+        return UIImage(named: "img_cona")!
+    }
+    
+    class var sportageImage: UIImage {
+        return UIImage(named: "img_sportage")!
+    }
+    
+    class var thenewavanteImage: UIImage {
+        return UIImage(named: "img_thenewavante")!
+    }
+    
+    class var tosanImage: UIImage {
+        return UIImage(named: "img_tosan")!
+    }
+    
+}
