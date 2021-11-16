ea217c5fd2877191b14d17f978c62832729fca86
beansBin <beans_bin@naver.com>
2021-11-16 23:25:19 +0900

    ➕[ADD] FilterVC 생성 (SOPT-29th-Joint-Seminar-10 #14)

---
+++ SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/ViewController/FilterVC.swift
@@ -0,0 +1,29 @@
+//
+//  FilterVC.swift
+//  SOPT-29th-JointSeminar10-iOS
+//
+//  Created by 박예빈 on 2021/11/16.
+//
+
+import UIKit
+
+class FilterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
