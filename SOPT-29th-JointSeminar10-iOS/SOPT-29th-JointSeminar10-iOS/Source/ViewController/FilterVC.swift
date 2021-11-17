bbacacce93caed301077230cda795f616654956c
beansBin <beans_bin@naver.com>
2021-11-17 12:16:39 +0900

    ✨[FEAT] filter, reservation 컬렉션뷰 xib 표시(SOPT-29th-Joint-Seminar-10#14)

--- SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/ViewController/FilterVC.swift
+++ SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/ViewController/FilterVC.swift
@@ -14,25 +14,37 @@ class FilterVC: UIViewController {
     @IBOutlet weak var filterCV: UICollectionView!
     @IBOutlet weak var reservationCV: UICollectionView!
     
+    let reserveContentList: [ReservationCarModel] = []
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setDelegate()
+        registerXib()
+    }
+    
+    // MARK: - Custom Methods
+    
+    func setDelegate() {
         filterCV.delegate = self
         filterCV.dataSource = self
         reservationCV.delegate = self
         reservationCV.dataSource = self
-        
-        registerXib()
     }
     
-    // TODO: - registerXib() 구현하기
+    func initAppContentList() {
+        
+        // TODO: - 서버 연결되면 데이터 넣기
+        
+    }
     
     func registerXib() {
-        let xibName = UINib(nibName: Const.Xib.NibName.reservationCVC, bundle: nil)
-        filterCV.register(xibName, forCellWithReuseIdentifier: Const.Xib.NibName.reservationCVC)
-        reservationCV.register(xibName, forCellWithReuseIdentifier: Const.Xib.NibName.reservationCVC)
+        let filterXibName = UINib(nibName: Const.Xib.NibName.filterCVC, bundle: nil)
+        filterCV.register(filterXibName, forCellWithReuseIdentifier: Const.Xib.NibName.filterCVC)
+        let reservationXibName = UINib(nibName: Const.Xib.NibName.reservationCVC, bundle: nil)
+        reservationCV.register(reservationXibName, forCellWithReuseIdentifier: Const.Xib.NibName.reservationCVC)
     }
 }
 
@@ -44,15 +56,54 @@ extension FilterVC: UICollectionViewDelegate {
 
 extension FilterVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return 6
+        if collectionView == filterCV {
+            return 6
+        } else {
+            
+            // TODO: - 서버 붙이면 reservationContentList.count로 변경하기
+            
+            return 10
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Const.Xib.NibName.reservationCVC, for: indexPath) as? ReservationCVC else {return UICollectionViewCell()}
-        
-        return cell
+        if collectionView == filterCV {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Const.Xib.NibName.filterCVC, for: indexPath) as? FilterCVC else {return UICollectionViewCell()}
+            
+            return cell
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Const.Xib.NibName.reservationCVC, for: indexPath) as? ReservationCVC else {return UICollectionViewCell()}
+            
+            return cell
+        }
+    }
+}
+
+extension FilterVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == filterCV {
+            
+            // TODO: - 내용에 따라 동적으로 크기 변경하기
+            
+            return CGSize(width: 70, height: 32)
+        } else {
+            return CGSize(width: 168, height: 168)
+        }
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.zero
+    }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        if collectionView == filterCV {
+            return 4
+        } else {
+            return 11
+        }
+    }
 }
