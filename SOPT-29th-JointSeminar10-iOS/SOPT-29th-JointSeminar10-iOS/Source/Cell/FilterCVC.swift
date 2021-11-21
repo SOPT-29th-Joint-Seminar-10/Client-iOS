42d89130f8207ac78c604386f83f067deb4453bc
beansBin <beans_bin@naver.com>
2021-11-21 23:00:34 +0900

    ✨[FEAT] FilterCVC setUI() 구현 (#15)

--- SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/Cell/FilterCVC.swift
+++ SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/Cell/FilterCVC.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class FilterCVC: UICollectionViewCell {
-
+    
+    @IBOutlet weak var titleButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setUI()
+    }
+    
+    func setUI() {
+        titleButton.translatesAutoresizingMaskIntoConstraints = false
+        titleButton.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        titleButton.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        titleButton.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.leftAnchor.constraint(equalTo: titleButton.trailingAnchor).isActive = true
+        closeButton.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        closeButton.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        closeButton.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
     }
 
 }
