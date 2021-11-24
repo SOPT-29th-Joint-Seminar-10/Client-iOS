d36ac492c566a9386d67ec3aaf6b63c65660e5a5
beansBin <beans_bin@naver.com>
2021-11-24 15:37:31 +0900

    ✅[CHORE] 필터 컬렉션뷰 레이아웃 조정 및 이미지 삽입 (#24)

--- SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/ViewController/FilterVC.swift
+++ SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/ViewController/FilterVC.swift
@@ -18,7 +18,7 @@ class FilterVC: UIViewController {
     let reserveContentList: [ReservationCarModel] = []
     var isClickedFilter: [Int] = [0, 0, 0, 0, 0, 0]
     let beforeFiltered: [String] = ["초기화", "대여기간", "차종", "지역", "가격", "인기"]
-    let afterFiltered: [String] = ["", "", "준중형", "서울/경기/인천", "낮은 가격 순", "인기"]
+    let afterFiltered: [String] = ["초기화", "3개월 | 2021", "준중형", "서울/경기/인천", "낮은 가격 순", "인기"]
     
     // MARK: - View Life Cycle
     
@@ -101,32 +101,44 @@ extension FilterVC: UICollectionViewDataSource {
            
             if isClickedFilter[indexPath.row] == 1 {
                 cell.closeButton.setImage(UIImage(systemName: "xmark"), for: .normal)
-                cell.closeButtonWidth.isActive = false
+                cell.closeButton.widthAnchor.constraint(equalToConstant: 37).isActive = true
+                cell.closeButton.leadingAnchor.constraint(equalTo: cell.titleButton.trailingAnchor, constant: -20).isActive = true
                 
                 let attribute = [NSAttributedString.Key.font: UIFont.cap1R, NSAttributedString.Key.foregroundColor: UIColor.white]
                 cell.titleButton.setAttributedTitle(NSAttributedString(string:afterFiltered[indexPath.row], attributes: attribute), for: .normal)
                 
-                cell.filterView.backgroundColor = UIColor.gray060
-                cell.filterView.layer.borderWidth = 0
+                cell.layer.backgroundColor = UIColor.gray060.cgColor
+                cell.layer.borderWidth = 0
+                cell.layer.cornerRadius = 0.042 * filterView.bounds.size.width
                 
                 cell.titleButton.setTitleColor(UIColor.white, for: .normal)
                 
                 let attributedTitle = cell.titleButton.attributedTitle(for: .normal)
-                cell.filterView.frame.size.width = attributedTitle!.size().width
+                cell.titleButton.frame.size.width = attributedTitle!.size().width + 25
+                cell.layer.frame.size.width = cell.titleButton.frame.size.width + 37
             } else {
-                cell.closeButton.frame.size.width = 0
                 cell.closeButton.setImage(UIImage(), for: .normal)
                 
                 let attribute = [NSAttributedString.Key.font: UIFont.cap1R, NSAttributedString.Key.foregroundColor: UIColor.gray060]
                 cell.titleButton.setAttributedTitle(NSAttributedString(string:beforeFiltered[indexPath.row], attributes: attribute), for: .normal)
                 
-                cell.filterView.backgroundColor = UIColor.white
-                cell.filterView.layer.borderWidth = 1.3
+                cell.layer.backgroundColor = UIColor.white.cgColor
+                cell.layer.borderWidth = 1.3
+                cell.layer.cornerRadius = 0.042 * filterView.bounds.size.width
+                cell.layer.borderColor = UIColor.gray040.cgColor
                 
                 cell.titleButton.setTitleColor(UIColor.gray060, for: .normal)
                 
                 let attributedTitle = cell.titleButton.attributedTitle(for: .normal)
-                cell.filterView.frame.size.width = attributedTitle!.size().width + 37
+                cell.layer.frame.size.width = attributedTitle!.size().width + 30
+                
+                if indexPath.row == 0{
+                    cell.titleButton.setImage(UIImage(named: "defaultHome"), for: .normal)
+                    cell.layer.frame.size.width = attributedTitle!.size().width + 30 + 15
+                } else if indexPath.row == 5 {
+                    cell.titleButton.setImage(UIImage(named: "group17"), for: .normal)
+                    cell.layer.frame.size.width = attributedTitle!.size().width + 30 + 15
+                }
             }
             
             return cell
@@ -145,35 +157,48 @@ extension FilterVC: UICollectionViewDelegateFlowLayout {
            
             if isClickedFilter[indexPath.row] == 1 {
                 cell!.closeButton.setImage(UIImage(systemName: "xmark"), for: .normal)
-                cell!.closeButtonWidth.isActive = false
+                cell!.closeButton.widthAnchor.constraint(equalToConstant: 37).isActive = true
+                cell!.closeButton.leadingAnchor.constraint(equalTo: cell!.titleButton.trailingAnchor, constant: -10).isActive = true
                 
                 let attribute = [NSAttributedString.Key.font: UIFont.cap1R, NSAttributedString.Key.foregroundColor: UIColor.white]
                 cell!.titleButton.setAttributedTitle(NSAttributedString(string:afterFiltered[indexPath.row], attributes: attribute), for: .normal)
-                
-                cell!.filterView.backgroundColor = UIColor.gray060
-                cell!.filterView.layer.borderWidth = 0
+               
+                cell!.layer.backgroundColor = UIColor.gray060.cgColor
+                cell!.layer.borderWidth = 0
+                cell!.layer.cornerRadius = 0.042 * filterView.bounds.size.width
+                cell!.layer.borderColor = UIColor.gray040.cgColor
                 
                 cell!.titleButton.setTitleColor(UIColor.white, for: .normal)
                 
                 let attributedTitle = cell!.titleButton.attributedTitle(for: .normal)
-                cell!.filterView.frame.size.width = attributedTitle!.size().width + 37
+                cell!.titleButton.frame.size.width = attributedTitle!.size().width + 25
+                cell!.layer.frame.size.width = cell!.titleButton.frame.size.width + 37
+                
             } else {
-                cell!.closeButton.frame.size.width = 0
                 cell!.closeButton.setImage(UIImage(), for: .normal)
                 
                 let attribute = [NSAttributedString.Key.font: UIFont.cap1R, NSAttributedString.Key.foregroundColor: UIColor.gray060]
                 cell!.titleButton.setAttributedTitle(NSAttributedString(string:beforeFiltered[indexPath.row], attributes: attribute), for: .normal)
                 
-                cell!.filterView.backgroundColor = UIColor.white
-                cell!.filterView.layer.borderWidth = 1.3
+                cell!.layer.backgroundColor = UIColor.white.cgColor
+                cell!.layer.borderWidth = 1.3
+                cell!.layer.cornerRadius = 0.042 * filterView.bounds.size.width
                 
                 cell!.titleButton.setTitleColor(UIColor.gray060, for: .normal)
                 
                 let attributedTitle = cell!.titleButton.attributedTitle(for: .normal)
-                cell!.filterView.frame.size.width = attributedTitle!.size().width
+                cell!.layer.frame.size.width = attributedTitle!.size().width + 30
+                
+                if indexPath.row == 0{
+                    cell!.titleButton.setImage(UIImage(named: "defaultHome"), for: .normal)
+                    cell!.layer.frame.size.width = attributedTitle!.size().width + 30 + 15
+                } else if indexPath.row == 5 {
+                    cell!.titleButton.setImage(UIImage(named: "group17"), for: .normal)
+                    cell!.layer.frame.size.width = attributedTitle!.size().width + 30 + 15
+                }
             }
             
-            return CGSize(width: cell!.filterView.frame.size.width + 37, height: 32)
+            return CGSize(width: cell!.layer.frame.size.width, height: 32)
         } else {
             guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return CGSize() }
             let numberOfCellsOfWidth: CGFloat = 2
