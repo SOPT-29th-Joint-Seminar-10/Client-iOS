730c692afe8af4350f6c0a5c9922563f9bab774d
beansBin <beans_bin@naver.com>
2021-11-29 00:44:25 +0900

    ➕[ADD] Extension Dictionary (#32)

---
+++ SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/Extension/Dictionary+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Dictionary+Extension.swift
+//  SOPT-29th-JointSeminar10-iOS
+//
+//  Created by 박예빈 on 2021/11/29.
+//
+
+import Foundation
+
+extension Dictionary where Key == String, Value == Any {
+    func toHTTPHeaders() -> HTTPHeaders {
+        HTTPHeaders(mapValues { String(describing: $0) })
+    }
+}
