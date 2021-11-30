30b938afb593efd1fb275b338a1f5b9dbb4d91d0
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-11-30 10:50:50 +0900

    ✅[CHORE] APIConstants에 recommendURL 추가 (#33)

---
+++ SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/NetworkModel/RecommendResponseData.swift
@@ -0,0 +1,8 @@
+//
+//  RecommendResponseData.swift
+//  SOPT-29th-JointSeminar10-iOS
+//
+//  Created by 정은희 on 2021/11/30.
+//
+
+import Foundation
---
+++ SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/NetworkService/RecommendDataService.swift
@@ -0,0 +1,8 @@
+//
+//  RecommendDataService.swift
+//  SOPT-29th-JointSeminar10-iOS
+//
+//  Created by 정은희 on 2021/11/30.
+//
+
+import Foundation
