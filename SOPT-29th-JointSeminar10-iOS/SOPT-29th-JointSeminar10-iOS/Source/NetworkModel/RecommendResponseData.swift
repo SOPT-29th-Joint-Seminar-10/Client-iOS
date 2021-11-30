e6b76fa886e5a20c74581a7c1c8975348eb24e4a
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-11-30 10:51:56 +0900

    ➕[ADD] RecommendResponseData.swift 파일 생성 (#33)

--- SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/NetworkModel/RecommendResponseData.swift
+++ SOPT-29th-JointSeminar10-iOS/SOPT-29th-JointSeminar10-iOS/Source/NetworkModel/RecommendResponseData.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+// MARK: - RecommendResponseData
+struct RecommendResponseData: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: [RecommendResultData]?
+}
+
+// MARK: - RecommendResultData
+struct RecommendResultData: Codable {
+    let carName, priceUnit: String
+    let price, discountRate: Int
+    let imageURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case carName, priceUnit, price, discountRate
+        case imageURL = "imageUrl"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        carName = (try? values.decode(String.self, forKey: .carName)) ?? ""
+        priceUnit = (try? values.decode(String.self, forKey: .priceUnit)) ?? ""
+        price = (try? values.decode(Int.self, forKey: .price)) ?? 0
+        discountRate = (try? values.decode(Int.self, forKey: .discountRate)) ?? 0
+        imageURL = (try? values.decode(String.self, forKey: .imageURL)) ?? ""
+    }
+}
+
+
